6c5d6a2af383d497d6a5f4873f934adc0976682d
Ruslan <ruslan231984@yandex.ru>
2025-06-23 16:17:59 +0300

    init

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/1', function (Request $request) {
+    return '111';
+});
