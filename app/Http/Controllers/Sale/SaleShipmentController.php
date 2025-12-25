8c702b97dd49af56cded91fc45ba65ad75f9508a
Ruslan <ruslan231984@yandex.ru>
2025-12-25 07:59:22 +0300

    fixed saleShipment controlller

--- app/Http/Controllers/Sale/SaleShipmentController.php
+++ app/Http/Controllers/Sale/SaleShipmentController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Sale;
 
 use App\Http\Controllers\Controller;
-use App\Models\Sale\SaleShipment\SaleShipment;
+use App\Models\Sale\SaleShipment;
 use App\Services\Sale\SaleShipment\SaleShipmentService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
